a38e85bf47f5635d35de5702cca1b74d72533c9e
Peter <tixiv@gmx.net>
2018-07-01 09:29:27 +0200

    refactored formatOne

--- Format/format.cpp
+++ Format/format.cpp
@@ -47,14 +47,30 @@ static float readFloat(const uint8_t * p)
 // "s32 0.01:0.2 Volt"
 // "f 10:Wert 0 ist %0.2 Volt"
 
-static int formatOne(QString &output, const QString &format, const uint8_t * data, int byteIndex, double * numOut)
+struct ParsedFormat
 {
+    int numBytes;
+    bool isFloat;
+    bool isSigned;
+    bool isBigEndian;
+    int numDigits;
+    double multiplier;
+    QString pre;
+    QString post;
+};
+
+static bool parseOne(ParsedFormat &pf, const QString &format)
+{
+    pf.numBytes = 0;
+    pf.isFloat = false;
+    pf.isSigned = false;
+    pf.isBigEndian = false;
+    pf.numDigits = 0;
+    pf.multiplier = 1.0;
+
     QStringList parts = format.split(':');
 
-    double num = 0.0;
-    int newByteIndex = byteIndex;
     QString multiplierPart;
-    int numDigits = 0;
 
     {
         // Parse the number specifier part
@@ -63,32 +79,28 @@ static int formatOne(QString &output, const QString &format, const uint8_t * dat
         QRegularExpression re("(u|U|s|S)(8|16|32)(.*)");
         QRegularExpressionMatch match = re.match(parts[0]);
         if (match.hasMatch()) {
-            int numBytes = match.captured(2).toInt() / 8;
-            newByteIndex += numBytes;
-            if(newByteIndex > 8)
-                return 8;
-            if(match.captured(1) == "u")
-                num = readUnsigned(&data[byteIndex], numBytes, false);
-            else if(match.captured(1) == "U")
-                num = readUnsigned(&data[byteIndex], numBytes, true);
-            else if(match.captured(1) == "s")
-                num = readSigned(&data[byteIndex], numBytes, false);
-            else if(match.captured(1) == "S")
-                num = readSigned(&data[byteIndex], numBytes, true);
+            pf.numBytes = match.captured(2).toInt() / 8;
+
+            if(match.captured(1) == "u"){
+
+            }else if(match.captured(1) == "U"){
+                pf.isBigEndian = true;
+            }else if(match.captured(1) == "s"){
+                pf.isSigned = true;
+            }else if(match.captured(1) == "S"){
+                pf.isSigned = true; pf.isBigEndian = true;
+            }
 
             multiplierPart = match.captured(3);
         }else{
             QRegularExpression re("f(.*)");
             QRegularExpressionMatch match = re.match(parts[0]);
             if(match.hasMatch()){
-                newByteIndex += 4;
-                if(newByteIndex > 8)
-                    return 8;
-                num = readFloat(&data[byteIndex]);
+                pf.numBytes = 4;
                 multiplierPart = match.captured(1);
-                numDigits = 2; // 2 default digits for float
+                pf.numDigits = 2; // 2 default digits for float
             }else{
-                return newByteIndex;
+                return false;
             }
         }
     }
@@ -98,34 +110,76 @@ static int formatOne(QString &output, const QString &format, const uint8_t * dat
         bool ok = false;
         double multiplier = multiplierPart.toDouble(&ok);
         if(ok){
-            num *= multiplier; // multiply the number
+            pf.multiplier = multiplier;
 
             while(multiplier < 1.0){
-                numDigits++;  // calculate an appropriate number of default digits
+                pf.numDigits++;  // calculate an appropriate number of default digits
                 multiplier *= 10.0;
             }
         }
     }
 
-    QString post;
     if(parts.size() > 1){
         // Handle format string if given
         QRegularExpression re("(.*?)%?\\d?\\.(\\d+)f?(.*)");
         QRegularExpressionMatch match = re.match(parts[1]);
         if(match.hasMatch()){
-            output += match.captured(1);
-            numDigits = match.captured(2).toInt();
-            post = match.captured(3);
+            pf.pre = match.captured(1);
+            pf.numDigits = match.captured(2).toInt();
+            pf.post = match.captured(3);
         }else{
-            output += parts[1];
+            pf.post = parts[1];
         }
     }
 
+    return true;
+}
+
+static int formatOne(QString &output, const QString &format, const uint8_t * data, int byteIndex, double * numOut)
+{
+
+    double num = 0.0;
+    int newByteIndex = byteIndex;
+
+    ParsedFormat pf;
+    if(parseOne(pf, format))
+    {
+        newByteIndex += pf.numBytes;
+        if(newByteIndex > 8){
+            newByteIndex = 8;
+            goto exit;
+        }
+
+        if(pf.isFloat){
+            num = readFloat(&data[byteIndex]);
+        }else{
+            if(pf.isSigned)
+            {
+                if(!pf.isBigEndian)
+                    num = readSigned(&data[byteIndex], pf.numBytes, false);
+                else
+                    num = readSigned(&data[byteIndex], pf.numBytes, true);
+            }
+            else
+            {
+                if(!pf.isBigEndian)
+                    num = readUnsigned(&data[byteIndex], pf.numBytes, false);
+                else
+                    num = readUnsigned(&data[byteIndex], pf.numBytes, true);
+            }
+        }
+
+        num *= pf.multiplier; // multiply the number
+
+        output += pf.pre;
+        output += QString().setNum(num, 'f', pf.numDigits);
+        output += pf.post;
+    }
+
+    exit:
     if(numOut)
         *numOut = num;
 
-    output += QString().setNum(num, 'f', numDigits);
-    output += post;
     return newByteIndex;
 }
 
